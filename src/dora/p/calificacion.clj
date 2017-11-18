bba330b1ceb33d7eeb72f7f175d2b7dbe2642fcb
Andres Gomez Urquiza <andres.gomez@datos.mx>
2017-11-17 20:57:04 -0600

    fix a la evaluación de oro, documentacion y estilo

--- src/dora/p/calificacion.clj
+++ src/dora/p/calificacion.clj
@@ -1,28 +1,38 @@
 (ns dora.p.calificacion)
 
-(defn broken-link-recommendation? [recommendations]
+(defn broken-link-recommendation?
+  "La liga no sirve?
+  Dentro del vector de recomendaciones se encuentra la de liga rota"
+  [recommendations]
   (seq (filter #(= "La URL no pudo ser leída por el robot" (:name %))
                    recommendations)))
 
-(defn falla-bronce [dataset resource recommendations]
-  (or (empty? (:issued dataset)) (empty? (:title dataset))
-      (empty? (:description dataset))
-      (empty? (:keyword dataset))
-      (empty? (:theme dataset))
+(defn falla-bronce
+  "Condiciones para que un recurso no pueda obtener el rango de Bronce"
+  [dataset resource recommendations]
+  (or (empty? (dataset :issued))
+      (empty? (dataset :title))
+      (empty? (dataset :description))
+      (empty? (dataset :keyword))
+      (empty? (dataset :theme))
       (broken-link-recommendation? recommendations)))
 
-(defn falla-plata [dataset resource recommendations]
+(defn falla-plata
+  "Condiciones para que un recurso no pueda obtener el rango de Plata"
+  [dataset resource recommendations]
   (or (nil? (-> dataset :publisher :name))
-      (nil? (-> dataset :publisher :mbox))
-      ; en el "algoritmo" original salia tmb funciona_la_url(dataset.publisher.url)
-      ))
+      (nil? (-> dataset :publisher :mbox))))
 
-(defn falla-oro [dataset resource recommendations]
-   ;cotejar contra la accrualPeriodicity
-  (empty? recommendations))
+(defn falla-oro
+  "Condiciones para que un recurso no pueda obtener el rango de Oro"
+  [dataset resource recommendations]
+  ;TODO cotejar contra la accrualPeriodicity
+  (not (empty? recommendations)))
 
-(defn calificacion [adela recommendations]
-  (let [dataset (:dataset adela)
+(defn calificacion
+  "Determina la calificación del recurso"
+  [adela recommendations]
+  (let [dataset  (:dataset adela)
         resource (:resource adela)]
     (cond (falla-bronce dataset resource recommendations) :none
           (falla-plata dataset resource recommendations) :bronce
