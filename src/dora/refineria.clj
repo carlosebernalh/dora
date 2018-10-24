07ad21801d256b7b338df82637997132cfb51746
Amilcar Paris Mandoki <amilcar.paris@gmail.com>
2018-10-24 14:22:07 -0500

    Nuevos datos SEP
    Se incluyen unos datos de SEP para hacer pruebas

--- src/dora/refineria.clj
+++ src/dora/refineria.clj
@@ -170,7 +170,10 @@
    ,"SNEDH-1cd40277-c0e4-4248-b10f-6a49f79caa15" "https://drive.google.com/uc?export=download&id=1uR7lyueZ2jREaMVKtfIffSrI5lEE5zeb"
    ,"SNEDH-678bb367-ca3c-44d4-9365-5e821be00c9d" "https://drive.google.com/uc?export=download&id=1tlSOH4OCH1Ki7WTtyPYen7gdPqy5RQD0"
    ,"SNEDH-9bbeb41f-ac51-4628-a01e-33ab8bb09357" "https://drive.google.com/uc?export=download&id=1DLQBKXao0xuA1FCeVjCsX9ythSteeNqF"
-   ,"SNEDH-9cb4002c-f00d-4860-a6e7-6ad42ecd217d" "http://datosdgai.cultura.gob.mx/pss/CiP01.csv"})
+   ,"SNEDH-9cb4002c-f00d-4860-a6e7-6ad42ecd217d" "http://datosdgai.cultura.gob.mx/pss/CiP01.csv"
+   ,"SNEDH-9e0cb023-3e34-4768-95a4-9d8512998033" "http://sep.gob.mx/dgticDatos/dgaigypt/ecP01.csv"
+   ,"SNEDH-c1ec6909-5149-4052-8a22-1c74f2a367b1" "http://sep.gob.mx/dgticDatos/dgaigypt/eaP05.csv"
+   ,"SNEDH-9e0cb023-3e34-4768-95a4-9d8512998033" "http://sep.gob.mx/dgticDatos/dgaigypt/ecP01.csv"})
 
 (defn apify-files []
   (doall
